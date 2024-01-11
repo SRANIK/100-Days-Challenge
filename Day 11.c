454f09095eca406a9fd918f8f1b533d9bbaf4313
SRANIK <100153827+SRANIK@users.noreply.github.com>
2024-01-11 23:26:09 +0530

    Update Day 11.c

--- Day 11.c
+++ Day 11.c
@@ -37,3 +37,22 @@ public:
         return climbStairs(n-1) + climbStairs(n-2);
     }
 };
+
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int n2 = 1; // n - 2
+        int n1 = 2; // n - 1
+
+        if(n <= 1) return 1;
+
+        for(int i = 3; i <= n; i++) {
+            int ni = n1 + n2;
+            n2 = n1; 
+            n1 = ni;
+        }
+
+        return n1;
+    }
+};
